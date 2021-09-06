a96fb992b3bd1c82459a2754d064c64f58ea9db0
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-06 15:46:35 +0400

    lesson 93. Хранение пароля в БД в не шифрованном формате

--- src/main/java/com/sai/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/sai/spring/security/configuration/MySecurityConfig.java
@@ -1,31 +1,39 @@
 package com.sai.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder
-                        .username("aydar")
-                        .password("aydar")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder
-                        .username("elena")
-                        .password("elena")
-                        .roles("HR"))
-                .withUser(userBuilder
-                        .username("ivan")
-                        .password("ivan")
-                        .roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
+        //        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder
+//                        .username("aydar")
+//                        .password("aydar")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder
+//                        .username("elena")
+//                        .password("elena")
+//                        .roles("HR"))
+//                .withUser(userBuilder
+//                        .username("ivan")
+//                        .password("ivan")
+//                        .roles("MANAGER", "HR"));
     }
 
     @Override
